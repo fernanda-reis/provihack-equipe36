c955cc082a9c478e921957cdd67dfaf01fce627d
Fernanda <fernandasoares.reis1@gmail.com>
2022-04-28 08:27:46 -0300

    @fernanda-reis [commit inicial]

---
+++ src/main/java/com/equipe36/provihack/ProvihackApplication.java
@@ -0,0 +1,13 @@
+package com.equipe36.provihack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvihackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProvihackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/equipe36/provihack/ProvihackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.equipe36.provihack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProvihackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
